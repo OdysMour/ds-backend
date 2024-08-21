a0b9373c9b67532a936a0034b8e1cd2de09bcce7
OdysMour <it21566@hua.gr>
2024-08-22 00:14:32 +0300

    Initial Commit

---
+++ src/test/java/gr/odys/ds_backend/DsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.odys.ds_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
